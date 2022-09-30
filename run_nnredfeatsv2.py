3ac665c543931d1738f121a5d58f3cdf03549482
Robbie Webbe <do19150@eduroam-233-149.nomadic.bris.ac.uk>
2022-09-30 09:55:01 +0100

    Minor addition to output files for choice of combinations NN run file.

--- run_nnredfeatsv2.py
+++ run_nnredfeatsv2.py
@@ -238,7 +238,7 @@ def build_test_NN(no_feats,combo_min=0,combo_max=10000):
         print('\nReal test completeness:', output_df.iloc[i,4])
         print('\nReal test purity:', output_df.iloc[i,5])
         
-    output_df.to_csv('NN_results/'+str(no_feats)+'feats_dt'+str(int(dt))+'_overall_accuracy.csv',index=False)
+    output_df.to_csv('NN_results/'+str(no_feats)+'feats_combos'+str(combo_min)+'-'+str(combo_max)+'_dt'+str(int(dt))+'_overall_accuracy.csv',index=False)
     
     return output_df
     

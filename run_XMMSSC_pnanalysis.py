8ccd03bc14df9cec28532ba508dbad7d88a55f00
Robbie Webbe <do19150@IT042780.local>
2022-12-01 12:03:53 +0000

    Improvement to XMMSSC output plot fnames.

--- run_XMMSSC_pnanalysis.py
+++ run_XMMSSC_pnanalysis.py
@@ -39,6 +39,8 @@ for i in range(no_objs):
     print(i,'/',no_objs)
     index = indices[i]
     
+    #pick out the source ID for collecting info on individual objects
+    srcid = str(cat[1].data.field('SRCID')[index])
     #pickout the obs id, and the source number in that obs
     obsid = str(cat[1].data.field('OBS_ID')[index])
     #convert the source number to hexadecimal for the download script
@@ -116,24 +118,24 @@ for i in range(no_objs):
                 pred_250 = dt250_prob_model.predict(feats_250)[0]
                 pred_1000 = dt1000_prob_model.predict(feats_1000)[0] 
                 
-                preds_dt250.append([obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
+                preds_dt250.append([srcid,obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
                                     'PN'+file[13],pred_250[1]])
-                preds_dt1000.append([obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
+                preds_dt1000.append([srcid,obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
                                     'PN'+file[13],pred_1000[1]])
                 
                 #if both predictions are greater than 90% QPE then the details to
                 #the strong candidate df output array
                 if pred_250[1] > 0.8 and pred_1000[1] > 0.8:
-                    top_cands.append([obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
+                    top_cands.append([srcid,obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
                                         'PN'+file[13],pred_250[1],pred_1000[1]])
                     #and save the plots to a folder
-                    outfile_name = obsid+'_'+srcnum+'_pn'+file[13]+'.pdf'
+                    outfile_name = cat[1].data.field('SRCID')[index]+'_'+obsid+'_'+srcnum+'_pn'+file[13]+'.pdf'
                     fig, axs = plt.subplots(2,1,sharex=True)
                     axs[0].plot(lc_250.time,lc_250.countrate,color='b')
                     axs[1].plot(lc_1000.time,lc_1000.countrate,color='b')
                     axs[0].set(ylabel='Count rate')
                     axs[1].set(xlabel='Time (s)',ylabel='Count rate')
-                    fig.suptitle('Observation '+obsid+' Source '+srcnum+' PN')
+                    fig.suptitle('SRCID '+srcid+' Observation '+obsid+' Source '+cat[1].data.field('SRC_NUM')[index]+' PN')
                     if pred_250[1] >= 0.99 and pred_1000[1] >= 0.99:
                         fig.savefig('4XMMSSC/top_cand_plots/conf_99/'+outfile_name)
                     elif pred_250[1] >= 0.95 and pred_1000[1] >= 0.95:
@@ -149,15 +151,15 @@ for i in range(no_objs):
         os.system('rm _dl_temp_/source_pnlc.tar')
         
         
-    # #if there is a M1 detection, perform the same analysis
+    # #if there is a M1 detection, try and download the data
     # if cat[1].data.field('CCDM1')[i] != -32768 :
     #     pn_url = "https://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno="+obsid+"&sourceno="+srcnum+"&level=PPS&instname=M1&extension=FTZ&name=SRCTSR"
     #     os.system('wget -O _dl_temp_/source_m1lc.tar "'+pn_url+'"')
-        
-    #     if os.path.getsize('_dl_temp_/source_m1lc.tar') != 0:
+    #     if os.path.getsize(os.getcwd()+'/_dl_temp_/source_m1lc.tar') != 0:
     #         os.chdir('_dl_temp_/')
     #         os.system('tar -xf source_m1lc.tar')
     #         os.chdir(os.getcwd()[:-10])
+            
     #         for file in os.listdir('_dl_temp_/'+obsid+'/pps/'):
     #             lc = XMMtolc('_dl_temp_/'+obsid+'/pps/'+file)
                 
@@ -169,14 +171,14 @@ for i in range(no_objs):
     #                 if lc == last_lc:
     #                     continue
                     
-    #             last_lc = lc
-                
+    #             last_lc = lc    
     #             if lc.dt > 250:
     #                 continue
     #             if lc.tseg < 50000:
     #                 continue
                 
     #             lc = lc.shift(-lc.time[0])
+                
     #             try:
     #                 lc = lc.truncate(start=15000,stop=(lc.time[-1]-15000),method='time')
     #             except:
@@ -196,29 +198,28 @@ for i in range(no_objs):
     #                 continue
     #             else:
     #                 lc_1000 = lc.rebin(1000)
-                    
+                                
     #             feats_250 = np.asarray([list(lcfeat([lc_250.time,lc_250.countrate],qpe='?'))])
     #             feats_1000 = np.asarray([list(lcfeat([lc_1000.time,lc_1000.countrate],qpe='?'))])
                 
     #             pred_250 = dt250_prob_model.predict(feats_250)[0]
-    #             pred_1000 = dt1000_prob_model.predict(feats_1000)[0]
+    #             pred_1000 = dt1000_prob_model.predict(feats_1000)[0] 
                 
-    #             preds_dt250.append([obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
+    #             preds_dt250.append([srcid,obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
     #                                 'M1'+file[13],pred_250[1]])
-    #             preds_dt1000.append([obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
+    #             preds_dt1000.append([srcid,obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
     #                                 'M1'+file[13],pred_1000[1]])
                 
     #             if pred_250[1] > 0.8 and pred_1000[1] > 0.8:
-    #                 top_cands.append([obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
+    #                 top_cands.append([srcid,obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
     #                                     'M1'+file[13],pred_250[1],pred_1000[1]])
-    #                 #and save the plots to a folder
-    #                 outfile_name = obsid+'_'+srcnum+'_m1'+file[13]+'.pdf'
+    #                 outfile_name = cat[1].data.field('SRCID')[index]+'_'+obsid+'_'+srcnum+'_m1'+file[13]+'.pdf'
     #                 fig, axs = plt.subplots(2,1,sharex=True)
     #                 axs[0].plot(lc_250.time,lc_250.countrate,color='b')
     #                 axs[1].plot(lc_1000.time,lc_1000.countrate,color='b')
     #                 axs[0].set(ylabel='Count rate')
     #                 axs[1].set(xlabel='Time (s)',ylabel='Count rate')
-    #                 fig.suptitle('Observation '+obsid+' Source '+srcnum+' M1')
+    #                 fig.suptitle('SRCID '+srcid+' Observation '+obsid+' Source '+cat[1].data.field('SRC_NUM')[index]+' M1')
     #                 if pred_250[1] >= 0.99 and pred_1000[1] >= 0.99:
     #                     fig.savefig('4XMMSSC/top_cand_plots/conf_99/'+outfile_name)
     #                 elif pred_250[1] >= 0.95 and pred_1000[1] >= 0.95:
@@ -228,22 +229,20 @@ for i in range(no_objs):
     #                 else:
     #                     fig.savefig('4XMMSSC/top_cand_plots/conf_80/'+outfile_name)
     #                 plt.close()
-                
+                                                    
     #         os.system('rm -r _dl_temp_/'+obsid+'/')
     #     os.system('rm _dl_temp_/source_m1lc.tar')
         
-
-    # #if there is a M2 detection, follow the same analysis
+    # #if there is a M1 detection, try and download the data
     # if cat[1].data.field('CCDM2')[i] != -32768 :
     #     pn_url = "https://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno="+obsid+"&sourceno="+srcnum+"&level=PPS&instname=M2&extension=FTZ&name=SRCTSR"
     #     os.system('wget -O _dl_temp_/source_m2lc.tar "'+pn_url+'"')
-        
-    #     if os.path.getsize('_dl_temp_/source_m2lc.tar') != 0:
+    #     if os.path.getsize(os.getcwd()+'/_dl_temp_/source_m2lc.tar') != 0:
     #         os.chdir('_dl_temp_/')
     #         os.system('tar -xf source_m2lc.tar')
     #         os.chdir(os.getcwd()[:-10])
+            
     #         for file in os.listdir('_dl_temp_/'+obsid+'/pps/'):
-    #             print(obsid,file)
     #             lc = XMMtolc('_dl_temp_/'+obsid+'/pps/'+file)
                 
     #             try:
@@ -253,15 +252,15 @@ for i in range(no_objs):
     #             else:
     #                 if lc == last_lc:
     #                     continue
-                
-    #             last_lc = lc
-                
+                    
+    #             last_lc = lc    
     #             if lc.dt > 250:
     #                 continue
     #             if lc.tseg < 50000:
     #                 continue
                 
     #             lc = lc.shift(-lc.time[0])
+                
     #             try:
     #                 lc = lc.truncate(start=15000,stop=(lc.time[-1]-15000),method='time')
     #             except:
@@ -281,30 +280,28 @@ for i in range(no_objs):
     #                 continue
     #             else:
     #                 lc_1000 = lc.rebin(1000)
-                
+                                
     #             feats_250 = np.asarray([list(lcfeat([lc_250.time,lc_250.countrate],qpe='?'))])
     #             feats_1000 = np.asarray([list(lcfeat([lc_1000.time,lc_1000.countrate],qpe='?'))])
                 
-    #             #find the prediction for the two feature sets and NNs
     #             pred_250 = dt250_prob_model.predict(feats_250)[0]
-    #             pred_1000 = dt1000_prob_model.predict(feats_1000)[0]
+    #             pred_1000 = dt1000_prob_model.predict(feats_1000)[0] 
                 
-    #             preds_dt250.append([obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
+    #             preds_dt250.append([srcid,obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
     #                                 'M2'+file[13],pred_250[1]])
-    #             preds_dt1000.append([obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
+    #             preds_dt1000.append([srcid,obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
     #                                 'M2'+file[13],pred_1000[1]])
                 
     #             if pred_250[1] > 0.8 and pred_1000[1] > 0.8:
-    #                 top_cands.append([obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
+    #                 top_cands.append([srcid,obsid,cat[1].data.field('SRC_NUM')[index],cat[1].data.field('EP_ONTIME')[index],
     #                                     'M2'+file[13],pred_250[1],pred_1000[1]])
-    #                 #and save the plots to a folder
-    #                 outfile_name = obsid+'_'+srcnum+'_m2'+file[13]+'.pdf'
+    #                 outfile_name = cat[1].data.field('SRCID')[index]+'_'+obsid+'_'+srcnum+'_m2'+file[13]+'.pdf'
     #                 fig, axs = plt.subplots(2,1,sharex=True)
     #                 axs[0].plot(lc_250.time,lc_250.countrate,color='b')
     #                 axs[1].plot(lc_1000.time,lc_1000.countrate,color='b')
     #                 axs[0].set(ylabel='Count rate')
     #                 axs[1].set(xlabel='Time (s)',ylabel='Count rate')
-    #                 fig.suptitle('Observation '+obsid+' Source '+srcnum+' M2')
+    #                 fig.suptitle('SRCID '+srcid+' Observation '+obsid+' Source '+cat[1].data.field('SRC_NUM')[index]+' M2')
     #                 if pred_250[1] >= 0.99 and pred_1000[1] >= 0.99:
     #                     fig.savefig('4XMMSSC/top_cand_plots/conf_99/'+outfile_name)
     #                 elif pred_250[1] >= 0.95 and pred_1000[1] >= 0.95:
@@ -314,20 +311,19 @@ for i in range(no_objs):
     #                 else:
     #                     fig.savefig('4XMMSSC/top_cand_plots/conf_80/'+outfile_name)
     #                 plt.close()
-                                        
-    #         os.system('rm -r _dl_temp_/'+obsid+'/')    
+                                                    
+    #         os.system('rm -r _dl_temp_/'+obsid+'/')
     #     os.system('rm _dl_temp_/source_m2lc.tar')
-
-
+        
 #send the data to an array
 preds_dt250 = np.asarray(preds_dt250)
 preds_dt1000 = np.asarray(preds_dt1000)
 top_cands = np.asarray(top_cands)
 
 #create the output dataframes
-predictions_dt250 = pd.DataFrame(data=preds_dt250, columns=['OBSID','SRC_NUM','EP_ONTIME','INST','QPE_CONF'],dtype=object)
-predictions_dt1000 = pd.DataFrame(data=preds_dt1000, columns=['OBSID','SRC_NUM','EP_ONTIME','INST','QPE_CONF'],dtype=object)
-top_candidates = pd.DataFrame(data=top_cands, columns=['OBSID','SRC_NUM','EP_ONTIME','INST','CONF_DT250','CONF_DT1000'])
+predictions_dt250 = pd.DataFrame(data=preds_dt250, columns=['SRCID','OBSID','SRC_NUM','EP_ONTIME','INST','QPE_CONF'],dtype=object)
+predictions_dt1000 = pd.DataFrame(data=preds_dt1000, columns=['SRCID','OBSID','SRC_NUM','EP_ONTIME','INST','QPE_CONF'],dtype=object)
+top_candidates = pd.DataFrame(data=top_cands, columns=['SRCID','OBSID','SRC_NUM','EP_ONTIME','INST','CONF_DT250','CONF_DT1000'])
 
 predictions_dt250.to_csv('4XMMSSC/predictions_pndt250.csv')
 predictions_dt1000.to_csv('4XMMSSC/predictions_pndt1000.csv')

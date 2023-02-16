a322ad7f1c091a95a500216e42042a45b1afefe2
Robbie Webbe <robbie.webbe@bristol.ac.uk>
2023-02-16 17:31:38 +0000

    Update for results for 6-8 features.

--- run_nnredfeatsv3.py
+++ run_nnredfeatsv3.py
@@ -17,7 +17,7 @@ import keras_tuner as kt
 from random import sample, shuffle
 from itertools import combinations
 
-dt = 1000
+dt = 250
 
 #import the training/validation data and the real & simulated testing data
 train_val_data = np.loadtxt(os.getcwd()+'/Features/train_val_data_dt'+str(int(dt))+'.csv',delimiter=',')

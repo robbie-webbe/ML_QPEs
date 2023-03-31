1fd51905885bdb07c0ef1c30dd77bca6235a988c
Robbie Webbe <do19150@eduroam-136-47.nomadic.bris.ac.uk>
2023-03-31 10:51:04 +0100

    Minor update.

--- add_poisson_noise.py
+++ add_poisson_noise.py
@@ -6,10 +6,21 @@ Created on Fri Mar 31 10:36:24 2023
 @author: do19150
 """
 
-lcs = np.loadtxt('LCGen/Diff_dt/no_qpe_sample_dt50.csv',delimiter=',')
+from scipy.stats import poisson
+import sys
+import os
+import numpy as np
+from tqdm import tqdm
+sys.path.append(os.getcwd())
+
+from lcfeaturegen import lcfeat
+
+dt=50
+
+lcs = np.loadtxt('LCGen/Diff_dt/no_qpe_sample_dt'+str(dt)+'.csv',delimiter=',')
 po_lcs = np.zeros(lcs.shape)
 po_lcs[0] = lcs[0]
-lcs_avg = np.average(lcs[1:])/1000
+lcs_avg = np.average(lcs[1:])/dt
 conversion = 1/lcs_avg
 for i in tqdm(range(len(lcs)-1)):
     raw_lc = lcs[i+1]
@@ -22,4 +33,4 @@ qpe_features = np.zeros((len(po_lcs)-1,15))
 for i in tqdm(range(50000)): 
     lc = [po_lcs[0],po_lcs[i+1]]
     qpe_features[i] = lcfeat(lc,qpe=0)
-np.savetxt('Features/no_qpe_feats_dt50_po1.csv',qpe_features,delimiter=',')
+np.savetxt('Features/no_qpe_feats_dt'+str(dt)+'_po1.csv',qpe_features,delimiter=',')

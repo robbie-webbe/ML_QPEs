8b3b767a97e14c7a9e058ca7bbc541d623e166b4
Robbie Webbe <do19150@IT042780.local>
2023-04-03 20:58:12 +0100

    Update to 14 feats dt1000 with new tf version.

--- run_nnredfeatsv2.py
+++ run_nnredfeatsv2.py
@@ -19,7 +19,7 @@ from random import sample, shuffle
 from itertools import combinations
 
 
-dt = 50
+dt = 250
 
 #import the training/validation data and the real & simulated testing data
 train_val_data = np.loadtxt(os.getcwd()+'/Features/train_val_data_dt'+str(int(dt))+'.csv',delimiter=',')
